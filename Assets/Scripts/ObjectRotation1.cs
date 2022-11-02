52e5de270f83dea88ed087f97acff95885ed51ae
dunfr <j01026708401@gmail.com>
2022-11-02 16:18:45 +0900

    11

--- Assets/Scripts/ObjectRotation1.cs
+++ Assets/Scripts/ObjectRotation1.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class ObjectRotation1 : MonoBehaviour
 {
+    [SerializeField]
+    private Physics2D playerPhysics2D;
     // Update is called once per frame
+    void Start()
+    {
+        playerPhysics2D = GetComponent<Physics2D>();    
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(IERotation());
-        }
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+            StartCoroutine(IERotation());                        
+         }
+
+        gravity();
     }
     IEnumerator IERotation()
     {
         for (int i = 0; i < 60; i++)
         {
-            transform.Rotate(0, 0, -3);
+            transform.Rotate(0, 0, 3);
             i++;
-            yield return new WaitForSeconds(0.01f);
+            yield return new WaitForSeconds(0.015f);
+        }
+    }
+    void gravity()
+    {
+        if (transform.rotation.z == 90)
+        {
+            
         }
     }
 }

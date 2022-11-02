92a39e7a14ff83e128e8c7586e8f0ff48b43897d
dunfr <j01026708401@gmail.com>
2022-11-02 14:38:10 +0900

    11

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,25 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    void FixedUpdate()
+    {
+        move();
+    }
     // Update is called once per frame
     void Update()
+    {
+    }
+    void move()
     {
         float horizontal = Input.GetAxis("Horizontal");
         Vector2 position = transform.position;
         position.x += horizontal * movementSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+    void jump()
+    {
+
     }
 }

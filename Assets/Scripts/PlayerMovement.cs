2d12ba6cd43d3468b60e2b6aeabe9205beabe200
dunfr <j01026708401@gmail.com>
2022-11-02 14:33:53 +0900

    11-02

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 position = transform.position;
+        position.x += horizontal * movementSpeed * Time.deltaTime;
+    }
+}

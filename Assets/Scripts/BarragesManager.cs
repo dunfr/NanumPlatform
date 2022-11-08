67b6a50ddc9f68306a6d7295c54da6290c9c2fe6
dunfr <j01026708401@gmail.com>
2022-11-08 22:09:07 +0900

    11

--- Assets/Scripts/BarragesManager.cs
+++ Assets/Scripts/BarragesManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class BarragesManager : MonoBehaviour
 {
+    public GameObject []barrage;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {

323c1dafb3f51464006cec1de22e1f9a9776ceb3
dunfr <j01026708401@gmail.com>
2022-11-16 02:36:26 +0900

    11

--- Assets/Scripts/TimelineManager.cs
+++ Assets/Scripts/TimelineManager.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
 public class TimelineManager : MonoBehaviour
 {
-    PlayableDirector playableDirector;
+    public TimelineAsset timelineAsset;
+    public GameObject note;
+    private PlayableDirector playableDirector;
     // Start is called before the first frame update
     void Start()
     {
         playableDirector = GetComponent<PlayableDirector>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        playableDirector.time = Singleton.Instance.musicTime;
+        //playableDirector.time = Singleton.Instance.musicTime;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Create(Singleton.Instance.musicTime);
+        }
         Debug.Log(playableDirector.time);
+    }
+    private void Create(float starttime)
+    {
+        var track = timelineAsset.CreateTrack<ActivationTrack>(null, "Activation Track");
+        playableDirector.SetGenericBinding(track, note);
+        var clip = track.CreateDefaultClip();
+        clip.start = 5;
+        clip.duration = 3f;
     }
 }
